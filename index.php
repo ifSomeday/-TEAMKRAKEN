96aeb887b9e000ff46ac51868f7fce085edad872
bmckalla <bmckalla@uvm.edu>
2016-04-02 09:56:34 -0400

    Base html

---
+++ index.php
@@ -0,0 +1,12 @@
+
+<?php
+include "top.php";
+?>
+
+	<article>
+		<h2>Sample Page</h2>
+	</article>
+
+<?php
+include "footer.php";
+?>
---
+++ nav.php
@@ -0,0 +1,16 @@
+<!-- ######################     Main Navigation   ########################## -->
+<nav>
+    <ol>
+        <?php
+       
+        
+            if ($path_parts['filename'] == "index") {
+                print '<li class="search"><a href="index.php">Search for a class</a></li>';
+            }
+        
+        ?>
+
+    </ol>
+</nav>
+<!-- #################### Ends Main Navigation    ########################## -->
+
